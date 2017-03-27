84e4a2ca1746a5e866fc8ea9cafc6534f8edf10e
felipepatente <felipepatente@yahoo.com.br>
2017-03-27 10:35:01 -0300

    Fazendo teste de repositorio

--- LojaVirtual/FrmLogin/ChamarTela.cs
+++ LojaVirtual/FrmLogin/ChamarTela.cs
@@ -16,7 +16,7 @@ namespace FrmLogin
 
             FrmClienteAletarCadastrar frmClienteAletarCadastrar = new FrmClienteAletarCadastrar();
             frmClienteAletarCadastrar.Show();
-            Console.WriteLine("Fazendo Teste no gitHub Mas");
+            Console.WriteLine("Fazendo Teste no gitHub");
         }
 
         public static void ClienteConsultarExcluir()
